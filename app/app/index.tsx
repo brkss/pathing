9e49479dc59c379459acb21f52fa432f984f14d6
Brahim Berkasse <berkassebrahim@gmail.com>
2024-03-01 17:52:35 +0100

    set home page layout

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,11 +1,32 @@
-import { Text } from 'react-native';
-
-
+import { View, Text, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 
 export default function Home(){
 
     return (
-        <Text>hello !</Text>
+       
+            <View style={styles.container}>
+               <View style={styles.circle} />
+            </View>
+       
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        marginTop: -200,
+        marginBottom: -200,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    circle: {
+        height: 180,
+        width: 180,
+        borderRadius: 200,
+        backgroundColor: 'blue'
+    }
+})
